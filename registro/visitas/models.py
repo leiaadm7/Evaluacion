061973c81a6507247861417e5b6366bea972eddb
leiaadm7 <leia.donoso@inacapmail.cl>
2025-09-25 00:52:26 -0300

    Codigo final
    Evaluacion 1

--- registro/visitas/models.py
+++ registro/visitas/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 #Modelo que guarda los datos de cada visita
 class Visita(models.Model):
     nombre = models.CharField(max_length=20)#Nombre del visitante
-    apellido = models.CharField(max_length=35, null=True, blank=True)#Apellido del visitante
+    apellido = models.CharField(max_length=35)#Apellido del visitante
     rut = models.CharField(max_length=12)#RUT del visitante (identificacion chilena)
     motivo = models.TextField()#Motivo de la visita
     hora_entrada = models.DateTimeField(auto_now_add=True)#Hora de entrada que se guarda automáticamente al crear el registro
@@ -13,3 +13,4 @@ class Visita(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.rut}"
+    
