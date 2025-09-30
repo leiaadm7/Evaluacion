a58ebc948d0e338866d73e0631db81277c72efd8
leiaadm7 <leia.donoso@inacapmail.cl>
2025-09-29 21:56:39 -0300

    .

--- registro/visitas/admin.py
+++ registro/visitas/admin.py
@@ -4,5 +4,5 @@ from .models import Visita
 # Configura cómo se verá el modelo Visita en el panel de administración
 @admin.register(Visita)
 class VisitaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'rut', 'motivo', 'hora_entrada', 'hora_salida')# columnas visibles
+    list_display = ('nombre', 'apellido','rut', 'motivo', 'hora_entrada', 'hora_salida')# columnas visibles
     list_filter = ('hora_entrada',) # filtros en el panel
