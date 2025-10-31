7e6cff9088128c0d5a2eea265fa7c9d53beddcfc
leiaadm7 <leia.donoso@inacapmail.cl>
2025-10-30 21:08:55 -0300

    .

--- registro/settings.py
+++ registro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%c(n_q@+t-!y8+h^b@o6p*s7l#f@k&c-w3j*z'
+SECRET_KEY = 'admin'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
