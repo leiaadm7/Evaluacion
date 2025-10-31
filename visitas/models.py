410e138d9dd4cd043efd6f6407f87041b8588aab
leiaadm7 <leia.donoso@inacapmail.cl>
2025-10-30 22:38:49 -0300

    evaluacion2 finaal?

--- visitas/models.py
+++ visitas/models.py
@@ -11,7 +11,13 @@ class Visita(models.Model):
     hora_entrada = models.DateTimeField(auto_now_add=True)#Hora de entrada que se guarda automáticamente al crear el registro
     hora_salida = models.DateTimeField(null=True, blank=True)#Hora de salida
     fecha = models.DateField(default=timezone.now)#Fecha de la visita automaticamente al crear el registro
-
+    estado = models.CharField(
+        max_length=20,
+        choices=[('EN_CURSO', 'En curso'), ('FINALIZADA', 'Finalizada')],
+        default='EN_CURSO'
+    )
+    registrado_por = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.rut}"
     
